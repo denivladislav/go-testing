56003c82d897e84a06c681358273e0efc9f2d1db
denivladislav <deni.vladislav@gmail.com>
2026-05-13 17:01:20 +0300

    feat: added IsEven

---
+++ even/even_test.go
@@ -0,0 +1,43 @@
+package even
+
+import (
+	"testing"
+)
+
+func assertEqual[T comparable](t *testing.T, got, want T) {
+	t.Helper()
+
+	if got != want {
+		t.Errorf("got = %v, want = %v", got, want)
+	}
+}
+
+func TestIsEven(t *testing.T) {
+	type test struct {
+		number	 int
+		expected bool
+	}
+
+	tests := map[string]test{
+		"determines positive even number": {
+			number: 1002,
+			expected: true,
+		},
+		"determines negative odd number": {
+			number: -11,
+			expected: false,
+		},
+		"treats zero as even number": {
+			number: 0,
+			expected: true,
+		},
+	}
+
+	for name, tt := range tests {
+		t.Run(name, func(t *testing.T) {
+			result := IsEven(tt.number)
+
+			assertEqual(t, result, tt.expected)
+		})
+	}
+}
